f8eb78c2cc391fdceab4f5fb170370a789ce4123
yuexiaona <18695812708@163.com>
2016-01-29 20:23:51 +0800

    初始化

---
+++ DictionaryChangeModel/DictionaryChangeModel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DictionaryChangeModel
+//
+//  Created by admin on 16/1/29.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DictionaryChangeModel/DictionaryChangeModel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DictionaryChangeModel
+//
+//  Created by admin on 16/1/29.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
